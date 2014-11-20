d10e562e9ad14bfd326360f5198bb9c3813e925e
Joshwa Fugett <jfugett27@gmail.com>
2014-11-20 00:47:37 -0800

    feat(performance): Increasing performance by making items sync

--- test/unit/index.js
+++ test/unit/index.js
@@ -42,8 +42,8 @@ describe('Timer', function() {
   });
 
   it('Should add a mark', function * () {
-    yield timer.start();
-    yield timer.mark('test');
+    timer.start();
+    timer.mark('test');
     timer._marks.length.should.be.equal(2);
   });
 
@@ -52,33 +52,34 @@ describe('Timer', function() {
   });
 
   it('Should run a generator', function * () {
-    yield timer2.run(function * () {
+    yield * timer2.run(function * () {
       return true;
     });
     timer2._marks.length.should.be.equal(2);
   });
 
   it('Should output the results in ms', function * () {
-    var results = yield timer2.result('ms');
+    var results = timer2.result('ms');
     results[0].unit.should.be.equal('ms');
   });
 
   it('Should output the result in seconds', function * () {
-    var results = yield timer2.result('s');
+    var results = timer2.result('s');
     results[0].unit.should.be.equal('s');
   });
 
   it('Should output the result in ns', function * () {
-    var results = yield timer2.result('ns');
+    var results = timer2.result('ns');
     results[0].unit.should.be.equal('ns');
   });
 
   it('Should be accurate', function * () {
-    yield timer3.start();
+    timer3.start();
     yield gen.delay(1000);
-    yield timer3.stop();
+    timer3.stop();
 
-    var results = yield timer3.result('s');
-    results[0].time.should.be.approximately(1, 0.05);
+    var results = timer3.result('s');
+    console.log();
+    results[0].time.should.be.approximately(1, 0.01);
   });
 });
